816fccdbf74a0bb106c8c1fe543f4418c1ec9539
alxtgda <alextgda@gmail.com>
2018-02-11 17:11:19 -0600

    Refactor setting out Form component out of App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
 import React, { Component } from "react";
-import { Form, Text } from "react-form";
 import logo from "./logo.svg";
+import NameForm from "./components/forms/GeneralDataForm";
 import "./App.css";
-
-class ExampleForm extends Component {
-	render() {
-		return (
-			<Form>
-				{formApi => (
-					<form onSubmit={formApi.submitForm} id="form1">
-						<label htmlFor="name">Name: </label>
-						<Text field="name" id="name" />
-						<label htmlFor="lastName">Last Name: </label>
-						<Text field="lastName" id="lastName" />
-						<button type="submit">Submit</button>
-					</form>
-				)}
-			</Form>
-		);
-	}
-}
 
 class App extends Component {
 	render() {
@@ -28,7 +10,7 @@ class App extends Component {
 				<header className="App-header">
 					<img src={logo} className="App-logo" alt="logo" />
 				</header>
-				<ExampleForm />
+				<NameForm />
 			</div>
 		);
 	}
---
+++ src/components/forms/GeneralDataForm.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { Form, Text } from "react-form";
+
+class NameForm extends Component {
+	render() {
+		return (
+			<div>
+				<Form>
+					{formApi => (
+						<form onSubmit={formApi.submitForm} id="form1">
+							<label htmlFor="name">Name: </label>
+							<Text field="name" id="name" />
+							<label htmlFor="lastName">Last Name: </label>
+							<Text field="lastName" id="lastName" />
+							<button type="submit">Submit</button>
+						</form>
+					)}
+				</Form>
+			</div>
+		);
+	}
+}
+export default NameForm;
