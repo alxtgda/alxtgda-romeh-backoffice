c515bf98d590cc9a07958ba0e13633c0f59b204a
alxtgda <alextgda@gmail.com>
2018-02-12 01:27:39 -0600

    Inserting Name into FireBase

--- src/App.js
+++ src/App.js
@@ -1,41 +1,25 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import NameForm from "./components/forms/GeneralDataForm";
 import "./App.css";
-import fire from "./firebase";
+import firebase from "firebase";
 
 class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			speed: 10
+	constructor(props) {
+		super(props);
+		var config = {
+			apiKey: "AIzaSyDfuy_aA3C6QpxGMPTgxrloPeQvDYYTDmo",
+			authDomain: "romeh-backoffice.firebaseapp.com",
+			databaseURL: "https://romeh-backoffice.firebaseio.com",
+			projectId: "romeh-backoffice",
+			storageBucket: "romeh-backoffice.appspot.com",
+			messagingSenderId: "906649784536"
 		};
-	}
-
-	componentDidMount() {
-		//creando referencia a la base de datos
-		const rootRef = fire
-			.database()
-			.ref()
-			.child("react");
-		const speedRef = rootRef.child("speed");
-		speedRef.on("value", snap => {
-			this.setState({
-				speed: snap.val()
-			});
-		});
-		// this.setState({
-		// 	speed: 25
-		// });
+		firebase.initializeApp(config);
 	}
 	render() {
 		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-				</header>
-				<NameForm />
-				<h1>{this.state.speed}</h1>
+			<div>
+				<NameForm db={firebase} label="Nombre: " />
 			</div>
 		);
 	}
--- src/components/forms/GeneralDataForm.js
+++ src/components/forms/GeneralDataForm.js
@@ -1,22 +1,47 @@
 import React, { Component } from "react";
-import { Form, Text } from "react-form";
 
 class NameForm extends Component {
+	constructor(props) {
+		super(props);
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.state = {
+			value: ""
+		};
+	}
+	handleChange(e) {
+		this.setState({
+			value: e.target.value
+		});
+	}
+	handleSubmit(e) {
+		alert("A name was submitted: " + this.state.value);
+		e.preventDefault();
+		let dbCon = this.props.db.database().ref("/names");
+		dbCon.push({
+			name: this.state.value
+		});
+		this.setState({
+			value: ""
+		});
+	}
 	render() {
 		return (
-			<div>
-				<Form>
-					{formApi => (
-						<form onSubmit={formApi.submitForm} id="nameForm">
-							<label htmlFor="name">Name: </label>
-							<Text field="name" id="name" />
-							<label htmlFor="lastName">Last Name: </label>
-							<Text field="lastName" id="lastName" />
-							<button type="submit">Submit</button>
-						</form>
-					)}
-				</Form>
-			</div>
+			<form onSubmit={this.handleSubmit}>
+				<div>
+					<div>
+						<label>{this.props.label}</label>
+					</div>
+					<input
+						name="firstName"
+						type="text"
+						placeholder="Escribe tu Nombre.."
+						onChange={this.handleChange}
+						value={this.state.value}
+					/>
+				</div>
+				<input type="submit" value="Submit" />
+			</form>
 		);
 	}
 }
